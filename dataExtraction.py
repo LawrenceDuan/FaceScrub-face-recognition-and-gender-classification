5b1bb0b08c693775114eb96ad4119f12dc96e4cc
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-15 15:06:20 -0400

    Bug fixed. Devision of 255 is only needed once at the final x_train.

--- dataExtraction.py
+++ dataExtraction.py
@@ -14,11 +14,11 @@ def prepare_training_data(data, required_actor_list):
     y_train = np.array([[1]*len(data[0])])
     for i in range(len(required_actor_list)):
         if i is 0:
-            x_train = np.vstack((x_train, data[required_actor_list[i]])) / 255
+            x_train = np.vstack((x_train, data[required_actor_list[i]]))
             x_train = np.delete(x_train, 0, axis=0)
             y_train = np.vstack((y_train, np.array([[i]*len(data[required_actor_list[i]])])))
             y_train = np.delete(y_train, 0, axis=0)
         else:
-            x_train = np.vstack((x_train, data[required_actor_list[i]])) / 255
+            x_train = np.vstack((x_train, data[required_actor_list[i]]))
             y_train = np.concatenate((y_train, np.array([[i]*len(data[required_actor_list[i]])])), axis=1)
     return x_train, y_train[0]
