d84947670fe0a758c048eefad1113daf4cf7bf2e
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-15 16:54:06 -0400

    New class. Accuracy computing.

---
+++ accuracy_compute.py
@@ -0,0 +1,13 @@
+
+
+def accuracy(y_valid, y_hypothesis):
+    # Compute accuracy
+    correct_count = 0
+    for i in range(len(y_valid)):
+        if y_hypothesis[i] <= 0.5 and y_valid[i] == 0: correct_count = correct_count + 1
+        if y_hypothesis[i] > 0.5 and y_valid[i] == 1:
+            correct_count = correct_count + 1
+    accuracy = correct_count / len(y_valid) * 100
+    # print(y_valid)
+    # print(y_hypothesis)
+    return accuracy
