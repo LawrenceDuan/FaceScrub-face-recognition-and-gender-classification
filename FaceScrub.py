463ab68b6a83c4d591664d2ebe6be1b5137a0b0d
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-15 15:07:53 -0400

    Add more command line arguments. Add constant value for classifier.

--- FaceScrub.py
+++ FaceScrub.py
@@ -19,6 +19,15 @@ def get_parser():
     parser.add_argument("-n",
                         "--number",
                         dest="number")
+    parser.add_argument("-a",
+                        "--alpha",
+                        dest="alpha")
+    parser.add_argument("-e",
+                        "--EPS",
+                        dest="EPS")
+    parser.add_argument("-i",
+                        "--iteration",
+                        dest="iteration")
     return parser
 
 
@@ -42,7 +51,7 @@ def part2():
     return im_data_training, im_data_validation, im_data_testing
 
 
-def part3():
+def part3(alpha, EPS, max_iters):
     '''
     Baldwin vs. Carell classification
     Build a classifier to distinguish pictures of Alec Baldwin from pictures of Steve Carell
@@ -52,14 +61,16 @@ def part3():
     im_data_training, im_data_validation, im_data_testing = part2()
     # Split out training data and label of Baldwin and Carell
     x_train, y_train = dataExtraction.prepare_training_data(im_data_training, [3,5])
+    # Add constant values for each image in x_train
+    x_train = np.concatenate((x_train, np.ones([x_train.shape[0], 1])), axis=1) / 255
 
-    # Theta initialization
-    theta0 = np.ones(1024) * 0.01
-    # theta0 = np.ones(1024) * 0.5
-    theta, costs, iters = lr.gradient_descent(lr.quadratic_cost_function, lr.derivative_quadratic_cost_function, x_train, y_train, theta0, 1e-5, 1e-6, 500)
-    # theta, costs, iters = lr.gradient_descent(lr.quadratic_cost_function, lr.derivative_quadratic_cost_function, x_train, y_train, theta0, 1e-5, 1e-6, 5000)
-    # theta, costs, iters = lr.gradient_descent(lr.quadratic_cost_function, lr.derivative_quadratic_cost_function, x_train, y_train, theta0, 1e-5, 1e-6, 50000)
+    # Theta initialization (1024 plus a constant theta)
+    theta0 = np.ones(1025) * 0.01
+    # theta0 = np.ones(1025) * 0.5
+    theta, costs, iters = lr.gradient_descent(lr.quadratic_cost_function, lr.derivative_quadratic_cost_function, x_train, y_train, theta0, alpha, EPS, max_iters)
     return theta, iters
+
+    # Plot the relationship between costs and iterations to see if there are any overfit, if the alpha is too large.
     # figure(1)
     # plot(iters, costs)
     # xlabel('iters')
@@ -69,15 +80,39 @@ def part3():
     # print(iters)
 
 
-def part41():
+def part4():
     '''
 
     :return:
     '''
-    theta, iters = part3()
-    new_theta = np.reshape(theta, (32, 32))
-    print(iters[len(iters)-1])
-    imshow(new_theta, cmap="RdBu", interpolation="spline16")
+    # Using full training set of Baldwin and Carell
+    # 500 iterations
+    theta_full, iters_full = part3(1e-5, 1e-6, 500)
+    # 5000 iterations
+    # theta_full, iters_full = part3(1e-5, 1e-6, 5000)
+    # 50000 iterations
+    # theta_full, iters_full = part3(1e-5, 1e-6, 50000)
+
+    # Using two images of Baldwin and Carell
+    # Get training data, validation data and testing data from part2
+    im_data_training, im_data_validation, im_data_testing = part2()
+    y_train = np.array([1, 1, 0, 0])
+    x_train = im_data_training[3][10]
+    x_train = np.vstack((x_train, im_data_training[3][11]))
+    x_train = np.vstack((x_train, im_data_training[5][20]))
+    x_train = np.vstack((x_train, im_data_training[5][21]))
+    # Add constant values for each image in x_train
+    x_train = np.concatenate((x_train, np.ones([x_train.shape[0], 1])), axis=1) / 255
+    # Theta initialization
+    theta0 = np.ones(1025) * 0.01
+    theta_2, costs, iters_2 = lr.gradient_descent(lr.quadratic_cost_function, lr.derivative_quadratic_cost_function, x_train, y_train, theta0, 1e-5, 1e-6, 50)
+
+    # Show image of theta
+    new_theta_full = np.reshape(theta_full[:1024], (32, 32))
+    imshow(new_theta_full, cmap="RdBu", interpolation="spline16")
+    show()
+    new_theta_2 = np.reshape(theta_2[:1024], (32, 32))
+    imshow(new_theta_2, cmap="RdBu", interpolation="spline16")
     show()
 
 
@@ -90,8 +125,7 @@ if __name__ == "__main__":
         #1: part1,
         2: part2,
         3: part3,
-        41: part41,
-        # 42: part42,
+        4: part4,
         #5: part5,
         #6: part6,
         #7: part7,
@@ -100,4 +134,7 @@ if __name__ == "__main__":
     # Get the function from switcher dictionary
     func = switcher.get(int(args.number), lambda: "Invalid number")
     # Execute the function
-    func()
+    if func is part3:
+        func(float(args.alpha), float(args.EPS), int(args.iteration))
+    else:
+        func()
