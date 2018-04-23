121bbd0b44eedf2f8753bfd2a9c5d0d172c85775
Cheng Duan <lawrence.chengduan@gmail.com>
2018-04-23 18:43:25 +0100

    New accuracy computing method.

--- accuracy_compute.py
+++ accuracy_compute.py
@@ -17,3 +17,27 @@ def accuracy(y_valid, y_hypothesis):
     # print(y_valid)
     # print(y_hypothesis)
     return accuracy
+
+
+def accuracy_2(y_valid, y_hypothesis):
+    '''
+    Compute classifier accuracy.
+    :param y_valid: validation set labels.
+    :param y_hypothesis: hypothesis set labels
+    :return: accuracy in %
+    '''
+    # Compute accuracy
+    correct_count = 0
+    for i in range(y_valid.shape[0]):
+        # tem_count = 0
+        # for j in range(y_valid.shape[1]):
+        #     if y_hypothesis[i][j] <= 0.5 and y_valid[i][j] == 0: tem_count = tem_count + 1
+        #     if y_hypothesis[i][j] > 0.5 and y_valid[i][j] == 1: tem_count = tem_count + 1
+        # if tem_count == 6: correct_count = correct_count + 1
+        for j in range(y_valid.shape[1]):
+            if y_hypothesis[i][j] <= 0.5 and y_valid[i][j] == 0: correct_count = correct_count + 1
+            if y_hypothesis[i][j] > 0.5 and y_valid[i][j] == 1: correct_count = correct_count + 1
+    accuracy = correct_count / y_valid.shape[0] * y_valid.shape[1]
+    # print(y_valid)
+    # print(y_hypothesis)
+    return accuracy
